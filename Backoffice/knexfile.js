50899755dc5b1f6c1528e58d9998c1ddf1f3385e
unknown <aluno20451@ipt.pt>
2020-03-10 23:43:40 +0000

    Adicionar db e knex
    Adição de uma base de dados sqlite3, e do módulo knex.
    Base de dados está no ficheiro db.
    Exemplo de knex está no "routes\users.js". Para mais exemplos de utilização do knex o bruno rosario forneceu este link útil https://devhints.io/knex.

---
+++ Backoffice/knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/mydb.db'
+    },
+    migrations: {
+      directory: './knex/migrations'
+    },
+    seeds: {
+      directory: './knex/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -1,13 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+//Knex initial config
+var dbConfig = {
+  client: 'sqlite3',
+    connection: {
+      filename: './db/mydb.db'
+    }
+};
+var knex = require('knex')(dbConfig);
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   res.send(req.url);
 });
 
+router.get("/:users", function(req, res, next) {
+  res.send("okay boomer");
+  //get em knex com arrow function
+  knex
+    .from('testes')
+    .select('*')
+    .then( (result)=>{console.log(result)} );
+  
 
-router.get('/:usersasdd', function(req, res, next) {
-  res.send(req.params.usersasdd);
 });
 module.exports = router;
