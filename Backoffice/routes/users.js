627cb667f0aafd0cf1410fb5678bcc6c3c30c0e4
unknown <aluno20451@ipt.pt>
2020-03-16 17:50:49 +0000

    Adição de endpoints para fazer registo e login.
    Adição de login e registo na rota "users.js"

--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -3,26 +3,71 @@ var router = express.Router();
 
 //Knex initial config
 var dbConfig = {
-  client: 'sqlite3',
-    connection: {
-      filename: './db/mydb.db'
-    }
+  client: "sqlite3",
+  connection: {
+    filename: "./db/mydb.db"
+  }
 };
-var knex = require('knex')(dbConfig);
+var knex = require("knex")(dbConfig);
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   res.send(req.url);
 });
 
-router.get("/:users", function(req, res, next) {
-  res.send("okay boomer");
-  //get em knex com arrow function
+router.get("/login/:user/:password", function(req, res, next) {
   knex
-    .from('testes')
-    .select('*')
-    .then( (result)=>{console.log(result)} );
-  
-
+    .from("users")
+    .select("*")
+    .where({ username: req.params.user })
+    .then(result => {
+      if (!result.length == 0) {
+        result = result[0];
+      }
+      if (
+        req.params.user == result.username &&
+        req.params.password == result.password
+      ) {
+        res.send(result);
+      } else {
+        let error = { error: "User or password incorret" };
+        res.send(error);
+      }
+    });
 });
+
+router.get("/register/:user/:password", function(req, res, next) {
+  knex
+    .from("users")
+    .select("*")
+    .where({ username: req.params.user })
+    .then(result => {
+      if (!result.length == 0) {
+        let error = { error: "User already exists", sucess: false };
+        res.send(error);
+        return;
+      }
+      console.log(result);
+      var chars =
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()";
+
+      var randomstring = "";
+
+      for (var i = 0; i < 64; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum, rnum + 1);
+      }
+      knex("users")
+        .insert({
+          username: req.params.user,
+          password: req.params.password,
+          token: randomstring,
+          role: "user"
+        })
+        .then(() => {
+          res.send({ error: "User created", sucess: true });
+        });
+    });
+});
+
 module.exports = router;
