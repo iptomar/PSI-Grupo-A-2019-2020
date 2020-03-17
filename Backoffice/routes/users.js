0c6a1d01a9df5524c28a8122523b8b0bcab1ddbd
unknown <aluno20451@ipt.pt>
2020-03-17 12:43:33 +0000

    Mudança do que o registo devolve
    Ao fazer o registo, caso seja bem sucedido, é retornada a entrada criada na bd.

--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var knex = require('../utils/databaseConection');
+var knex = require("../utils/databaseConection");
 
 /* GET users listing. */
 router.get("/", async function(req, res, next) {
@@ -9,66 +9,72 @@ router.get("/", async function(req, res, next) {
 });
 
 router.get("/login/:user/:password", async function(req, res, next) {
+  let query;
   await knex
     .from("users")
     .select("*")
     .where({ username: req.params.user })
     .then(result => {
-      if (!result.length == 0) {
-        result = result[0];
-      }
-      if (
-        req.params.user == result.username &&
-        req.params.password == result.password
-      ) {
-        res.send(result);
-      } else {
-        let error = { error: "User or password incorret" };
-        res.send(error);
-      }
-    }
-    )
-    .catch(function() {
-      res.send("An error occurred");
-      })
-    ;
-});
-
-router.get("/register/:user/:password", async function(req, res, next) {
-  await knex
-    .from("users")
-    .select("*")
-    .where({ username: req.params.user })
-    .then(result => {
-      if (!result.length == 0) {
-        let error = { error: "User already exists", sucess: false };
-        res.send(error);
-        return;
-      }
-      console.log(result);
-      var chars =
-        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()";
-
-      var randomstring = "";
-
-      for (var i = 0; i < 64; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum, rnum + 1);
-      }
-      knex("users")
-        .insert({
-          username: req.params.user,
-          password: req.params.password,
-          token: randomstring,
-          role: "user"
-        })
-        .then(() => {
-          res.send({ error: "User created", sucess: true });
-        });
+      query = result;
     })
     .catch(function() {
       res.send("An error occurred");
-      });
+    });
+    if (!query.length == 0) {
+      query = query[0];
+    }
+    if (
+      req.params.password == query.password
+    ) {
+      res.send(query);
+    } else {
+      let error = { error: "User or password incorret" };
+      res.send(error);
+    }
+});
+
+router.get("/register/:user/:password", async function(req, res, next) {
+  let query;
+  await knex
+    .from("users")
+    .select("*")
+    .where({ username: req.params.user })
+    .then(result => {
+      query = result;
+    })
+    .catch(function() {
+      res.send("An error occurred");
+    });
+  if (!query.length == 0) {
+    let error = { error: "User already exists", sucess: false };
+    res.send(error);
+    return;
+  }
+  var chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()";
+
+  var randomstring = "";
+
+  for (var i = 0; i < 64; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum, rnum + 1);
+  }
+
+  await knex("users").insert({
+    username: req.params.user,
+    password: req.params.password,
+    token: randomstring,
+    role: "user"
+  });
+
+  await knex("users")
+  .select("*")
+  .where({ username: req.params.user })
+  .then((select)=>{
+    console.log(select);
+    select = select[0];
+    res.send(select);
+  })
 });
 
 module.exports = router;
