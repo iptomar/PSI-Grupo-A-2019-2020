397b35ee9fae4cbe4a4ea04615b8c27c02908358
João Silva <48324498+aluno20451@users.noreply.github.com>
2020-03-18 16:05:12 +0000

    Necessario token para fazer registo de utilizador
    Tem que ser enviado um token via body no register para verificar se o utilizador que está a criar um novo user é o admin.

--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -18,63 +18,41 @@ router.post("/login", async function(req, res, next) {
       if (!result.length == 0) {
         result = result[0];
       }
-      if (
-        req.body.user == result.username &&
-        req.body.password == result.password
-      ) {
+      if (req.body.password == result.password) {
         res.send(result);
       } else {
         let error = { error: "User or password incorret" };
         res.send(error);
       }
-    }
-    )
-    .catch(async function(err){
+    })
+    .catch(async function(err) {
       res.send("An error occurred");
     });
-  if (!query.length == 0) {
-    query = query[0];
-  }
-  if (req.params.password == query.password) {
-    res.send(query);
-  } else {
-    let error = { error: "User or password incorret" };
-    res.send(error);
-  }
 });
 
 router.post("/register", async function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  await knex("users")
+    .select("*")
+    .where({username: "admin"})
+    .then(result =>{
+      result = result[0];
+      // se o token enviado não for o de admin, retorna
+      if(req.body.token != result.token){
+        let error = { error: "The token provided is not the admin's", sucess: false };
+        res.send(error);
+        return;
+      }
+    });
+
+  let query;
+  // verifica se o utilizador já existe, se já devolve um erro
   await knex
     .from("users")
     .select("*")
     .where({ username: req.body.user })
     .then(result => {
-      if (!result.length == 0) {
-        let error = { error: "User already exists", sucess: false };
-        res.send(error);
-        return;
-      }
-      console.log(result);
-      var chars =
-        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()";
-
-      var randomstring = "";
-
-      for (var i = 0; i < 64; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum, rnum + 1);
-      }
-      knex("users")
-        .insert({
-          username: req.body.user,
-          password: req.body.password,
-          token: randomstring,
-          role: "user"
-        })
-        .then(() => {
-          res.send({ error: "User created", sucess: true });
-        });
+      query = result;
     })
     .catch(async function(err) {
       res.send("An error occurred");
@@ -84,6 +62,7 @@ router.post("/register", async function(req, res, next) {
     res.send(error);
     return;
   }
+  // criação da chave do utilizador
   var chars =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()";
 
@@ -95,15 +74,15 @@ router.post("/register", async function(req, res, next) {
   }
 
   await knex("users").insert({
-    username: req.params.user,
-    password: req.params.password,
+    username: req.body.user,
+    password: req.body.password,
     token: randomstring,
     role: "user"
   });
 
   await knex("users")
     .select("*")
-    .where({ username: req.params.user })
+    .where({ username: req.body.user })
     .then(select => {
       console.log(select);
       select = select[0];
