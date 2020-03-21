90065b0836ae9b0572f34c8d2ab81c3fb6e50c80
aluno20619 <48324470+aluno20619@users.noreply.github.com>
2020-03-21 17:29:32 +0000

    Add files via upload

--- Backoffice/utils/seeds/04_interesse.js
+++ Backoffice/utils/seeds/04_interesse.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('Roteiro').del()
+  return knex('Interesse').del()
   .then(function () {
     // Inserts seed entries
-    return knex('Roteiro').insert([
+    return knex('Interesse').insert([
       {
         id: '1',
         titulo: 'Palácio da Justiça',
---
+++ Backoffice/utils/seeds/05_inter_roteir.js
@@ -0,0 +1,21 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('Inter_Roteir').del()
+  .then(function () {
+    // Inserts seed entries
+    return knex('Inter_Roteir').insert([
+      {
+        id_inter: '1',
+        id_roteir: '1'
+	  },
+      {
+        id_inter: '2',
+        id_roteir: '1'
+		},
+      {
+        id_inter: '3',
+        id_roteir: '1'
+		}
+    ]);
+  });
+};
