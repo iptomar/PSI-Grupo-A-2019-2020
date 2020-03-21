57ca002aec8a390536428bbaa9fd760ac40f5fd1
aluno20619 <48324470+aluno20619@users.noreply.github.com>
2020-03-21 16:58:05 +0000

    Add files via upload

---
+++ Backoffice/utils/seeds/03_roteiro.js
@@ -0,0 +1,24 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('Roteiro').del()
+  .then(function () {
+    // Inserts seed entries
+    return knex('Roteiro').insert([
+      {
+        id: '1',
+        nome: 'Roteiro dos Monumentos',
+        descricao: 'Se tem algo que eu adoro no centro oeste de Portugal é poder andar poucos quilômetros entre suas cidades. Tomar é um desses casos.'
+      },
+      {
+        id: '2',
+        nome: 'Roteiro dos Bares',
+        descricao: 'Restaurantes, bares, loja de discos, cafés, lojas de decoração  trouxeram uma vida de agitação constante e não faltam sítios para exercitar o cotovelo. '
+	  },
+      {
+        id: '3',
+        nome: 'Roteiro de Tomar',
+        descricao: 'A cidade de Tomar, merece, sem dúvida alguma, uma visita, pela sua riqueza artística e cultural. Esta cidade encantadora, banhada pelo Rio Nabão, promete um dia em cheio. '
+      }
+    ]);
+  });
+};
