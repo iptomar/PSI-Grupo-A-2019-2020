3ca3e4122e14f002281f679a347b37bcc87662e0
Pedro Vinha <48485552+Setraick@users.noreply.github.com>
2020-03-22 15:50:35 +0000

    Update 06_roteir_user.js

--- Backoffice/utils/seeds/06_roteir_user.js
+++ Backoffice/utils/seeds/06_roteir_user.js
@@ -5,16 +5,16 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     return knex('Roteir_User').insert([
       {
-        user_id: '1',
-        id_roteir: '1'
+        user_id: 1,
+        id_roteir: 1
 	  },
       {
-        user_id: '2',
-        id_roteir: '1'
+        user_id: 2,
+        id_roteir: 1
 		},
       {
-        user_id: '3',
-        id_roteir: '1'
+        user_id: 3,
+        id_roteir: 1
 		}
     ]);
   });
