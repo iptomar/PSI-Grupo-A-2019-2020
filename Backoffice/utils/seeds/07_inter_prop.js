34822b340b7b5a68260f4d55cc970e52f10eb888
Pedro Vinha <48485552+Setraick@users.noreply.github.com>
2020-03-22 15:50:01 +0000

    Update 07_inter_prop.js

--- Backoffice/utils/seeds/07_inter_prop.js
+++ Backoffice/utils/seeds/07_inter_prop.js
@@ -5,16 +5,16 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     return knex('Inter_Prop').insert([
       {
-        prop_id: '1',
-        id_inter: '1'
+        prop_id: 1,
+        id_inter: 1
 	  },
       {
-        prop_id: '2',
-        id_inter: '1'
+        prop_id: 2,
+        id_inter: 1
 		},
       {
-        prop_id: '3',
-        id_inter: '1'
+        prop_id: 3,
+        id_inter: 1
 		}
     ]);
   });
