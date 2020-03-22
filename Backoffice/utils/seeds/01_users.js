ecc0105d1af5343c1f97529f8ef019deff1782c6
Pedro Vinha <48485552+Setraick@users.noreply.github.com>
2020-03-22 16:20:07 +0000

    Update 01_users.js

--- Backoffice/utils/seeds/01_users.js
+++ Backoffice/utils/seeds/01_users.js
@@ -6,18 +6,27 @@ exports.seed = function(knex, Promise) {
     return knex('users').insert([
       {
         id: 1,
-        email: 'a@a.a',
-        password: 'a'
+        email: 'a@email.com',
+        password: 'a',
+        first_name: "Jacinto",
+        last_name: "Ribeiro" ,
+        age: 21
       },
       {
         id: 2,
-        email: 'b@b.b',
-        password: 'b'
+        email: 'b@email.com',
+        password: 'b',
+        first_name: "Carolina",
+        last_name: "Silva" ,
+        age: 34
       },
       {
-        id: 3
-        email: 'c@c.c',
-        password: 'c'
+        id: 3,
+        email: 'c@email.com',
+        password: 'c',
+        first_name: "Hugo",
+        last_name: "Oliveira" ,
+        age: 52
       }
     ]);
   });
