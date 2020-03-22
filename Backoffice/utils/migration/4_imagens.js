7b21a31687a1cd12b3b7ac41434b3f8568c8549a
Pedro Vinha <48485552+Setraick@users.noreply.github.com>
2020-03-22 15:07:31 +0000

    Update 4_imagens.js

--- Backoffice/utils/migration/4_imagens.js
+++ Backoffice/utils/migration/4_imagens.js
@@ -1,9 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('images', (table) => {
     table.increments('id').primary()
-    table.string('path')
-    table.string('legenda')
-    table.string('autorFonte')
+    table.string('Path')
+    table.string('Legenda')
+    table.string('AutorFonte')
     table.string('Interesse_id')
     table.foreign('Interesse_id').references('Interesse.id')
   })
