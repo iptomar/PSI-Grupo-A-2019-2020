35a1686d9d85815116533109441edd395b0c0cca
João Silva <48324498+aluno20451@users.noreply.github.com>
2020-03-22 20:35:17 +0000

    Token de admin adicionado

--- Backoffice/utils/seeds/01_users.js
+++ Backoffice/utils/seeds/01_users.js
@@ -6,31 +6,40 @@ exports.seed = function(knex, Promise) {
     return knex('users').insert([
       {
         id: 1,
+        email: 'admin@admin.com',
+        password: 'admin',
+        first_name: "admin",
+        last_name: "admin" ,
+        age: 20,
+        token: "K(+?y/(Le0lMnpP+!vZ)GQToI=WesVRXapAc21AXqXx*M8S78KTgx7i-vn)dUu?0"
+      },
+      {
+        id: 2,
         email: 'a@email.com',
         password: 'a',
         first_name: "Jacinto",
         last_name: "Ribeiro" ,
         age: 21,
-        token: ""
+        token: "/qp*NHi7Pk9ROCCKcN(UlyD28!*e5?(g1EXRJtx+dGT/QdH3ANKT*-uvNQfhmk6h"
 
       },
       {
-        id: 2,
+        id: 3,
         email: 'b@email.com',
         password: 'b',
         first_name: "Carolina",
         last_name: "Silva" ,
         age: 34,
-        token: ""
+        token: "rK/+pz!d?d&n+?3*7Q*mKBcVT1TZw!vDEkJfZPFFCWnG&Kq4&lQqq3SH*ZeW7f9Q"
       },
       {
-        id: 3,
+        id: 4,
         email: 'c@email.com',
         password: 'c',
         first_name: "Hugo",
         last_name: "Oliveira" ,
         age: 52,
-        token: ""
+        token: "P6+aRiq38s*zRrE7ciqzWo&JP*R!25h+H8p/QOR9/V)pZ)+z!&7c/*Oc&3ch5MGg"
       }
     ]);
   });
