4e4337f048143221ce4539a213e4f2709ae2bea9
Pedro Vinha <48485552+Setraick@users.noreply.github.com>
2020-03-22 15:26:12 +0000

    Update 3_prop.js

--- Backoffice/utils/migration/3_prop.js
+++ Backoffice/utils/migration/3_prop.js
@@ -4,9 +4,9 @@ exports.up = (knex, Promise) => {
     table.string('first_name')
     table.string('last_name')
     table.string('user_id')
-    table.string('guide_id')
+    table.string('owner_id')
     table.foreign('user_id').references('users.id')
-    table.foreign('guide_id').references('Roteiro.id')
+    table.foreign('owner_id').references('Interesse.id')
   })
 }
 
