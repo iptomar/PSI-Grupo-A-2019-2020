c0062f8cfc1c7280f47b98053685d20569a24cc0
Difficulty01 <bruno.silva.rosario@gmail.com>
2020-03-23 23:06:50 +0000

    users update
    endpoint de atualização dos utilizadores

--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -97,4 +97,63 @@ router.post("/register", async function(req, res, next) {
       res.send(select);
     });
 });
+
+router.post("/login", async function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.body);
+
+  await knex
+    .from("users")
+    .select("*")
+    .where({ username: req.body.user })
+    .then(result => {
+      if (!result.length == 0) {
+        result = result[0];
+      }
+      if (req.body.password == result.password) {
+        res.send(result);
+      } else {
+        let error = { error: "User or password incorret" };
+        res.send(error);
+      }
+    })
+    .catch(async function(err) {
+      console.log(err);
+      res.send(err)
+    });
+});
+
+//usage:
+//body.user = token
+//body.data = informação de atualização(json)
+router.post("/update", async function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.body);
+  for(var key in req.body.data)
+  if(key == "id" || key == "token")
+  {
+    let errormesage = { sucess : false , mesage: "WE CANT UPDATE THIS"};
+    res.send(errormesage);
+  }else{
+  try{
+  var d = new Date();
+  await file("logs/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate(), "a",JSON.stringify(req.body)+""+JSON.stringify(req.params)+""+JSON.stringify(req.baseUrl));
+  var admin = false;
+      return await knex('users').where({ token: req.body.user }).update(req.body.data).then(async function( resp ){ 
+        let errormesage = { sucess : true , mesage: "update sucessfull" };
+        res.send(errormesage);
+    }).catch(async function(err) {
+      await file("error/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+d.getUTCHours()+"_"+d.getUTCMinutes()+"_"+d.getUTCSeconds(), "a",""+err.stack);
+      let errormesage = { sucess : false , mesage: "token not used" };
+      res.send(errormesage);
+        });
+
+  }
+  catch (err){
+    await file("error/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+d.getUTCHours()+"_"+d.getUTCMinutes()+"_"+d.getUTCSeconds(), "a",""+err.stack);
+    let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
+    res.send(errormesage);
+  }
+
+}});
 module.exports = router;
