1ff87222464b2393420e9224800d30c89893e878
Miguel Pereira <aluno18561@ipt.pt>
2020-03-27 11:53:27 +0000

    Added points router. Added delete endpoint

--- Backoffice/app.js
+++ Backoffice/app.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pointsRouter = require('./routes/points');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/points', pointsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Backoffice/routes/points.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+var knex = require("../utils/databaseConection");
+
+//Usage:
+//body.data = id do ponto a eliminar(json)
+router.post("/delete", async function(req, res, next){
+    //ToDo: Terá de ser verificado se o utilizador a solicitar o delete é
+    //um administrador ou o utilizador que o criou
+
+    let del = false;
+
+    //Verificar se o ponto existe
+    await knex('Interesse')
+    .where({id: req.body.id})
+    .then(result => {
+        if(!result.length == 0){
+            del = true;
+        } else {
+            let errormesage = {sucess: false, mesage: "Point doesn't exist"};
+            res.send(errormesage);
+        }
+    })
+    .catch(async function(err){
+        await file("error/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+d.getUTCHours()+"_"+d.getUTCMinutes()+"_"+d.getUTCSeconds(), "a",""+err.stack);
+      let errormesage = { sucess : false , mesage: "token not used" };
+      res.send(errormesage);
+    });
+
+    //Eliminar o ponto
+    if(del){
+        await knex('Interesse')
+        .where({id: req.body.id})
+        .del();
+    }
+
+    let errormesage= {sucess: true, mesage: "Point sucessfully deleted"};
+    res.send(errormesage);
+});
+
+module.exports = router;
