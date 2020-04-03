4d7f3c889644b5a6b283126e88f34f7aa38b650a
Miguel Pereira <aluno18561@ipt.pt>
2020-04-03 13:02:59 +0100

    props insert endpoint

--- Backoffice/app.js
+++ Backoffice/app.js
@@ -10,6 +10,7 @@ dotenv.config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pointsRouter = require('./routes/points');
+var propsRouter = require('./routes/props');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/points', pointsRouter);
+app.use('/props', propsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
