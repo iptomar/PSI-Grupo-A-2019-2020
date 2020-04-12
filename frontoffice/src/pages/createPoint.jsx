2b60ee5d46a6d765c5c3d8b0da0e075746302787
Goncalo Rocha <48484505+gonzaleZayy@users.noreply.github.com>
2020-04-12 01:15:56 +0100

    Criação dos pontos corrigido

--- frontoffice/src/pages/createPoint.jsx
+++ frontoffice/src/pages/createPoint.jsx
@@ -7,7 +7,7 @@ class InsertPoint extends React.Component {
         super(props);
         this.state = {
             redirect: "",
-
+            success: null
         };
 
     }
@@ -17,6 +17,12 @@ class InsertPoint extends React.Component {
         let description = document.getElementById("descr").value;
         let tipoEdificio = document.getElementById("tipoEdif").value;
         let dataEdif = document.getElementById("date").value;
+        let coordinates = document.getElementById("coordinates").value;
+
+        if(titulo == "" || description == "" || tipoEdificio == "" || dataEdif=="" || coordinates == ""){
+            this.setState({ success: false });
+            return;
+        }
 
         let user = JSON.parse(sessionStorage.getItem("userData"));
 
@@ -24,32 +30,39 @@ class InsertPoint extends React.Component {
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-type", "application/json");
         var raw = JSON.stringify({
-          "data":{
-            "titulo": titulo,
-            "descricao": description,
-            "coordenadas": '0',
-            "data": dataEdif,
-            "tipoEdif": tipoEdificio,
-            "user_id": user.id,
-            "prop_id":1
-          }
+            "data": {
+                "titulo": titulo,
+                "descricao": description,
+                "coordenadas": coordinates,
+                "data": dataEdif,
+                "tipoEdif": tipoEdificio,
+                "user_id": user.id,
+                "prop_id": 1
+            }
         });
-    
+
         var requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          mode: "cors",
-          redirect: "follow"
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            mode: "cors",
+            redirect: "follow"
         };
-    
+
         let response = await fetch(
-          "https://localhost:3000/points/insert",
-          requestOptions
+            "https://localhost:3000/points/insert",
+            requestOptions
         )
 
         let data = await response.json();
+
         console.log(data);
+
+        if (data.sucess === true) {
+            this.setState({ success: true });
+        } else {
+            this.setState({ success: false });
+        }
     }
 
     render() {
@@ -112,121 +125,13 @@ class InsertPoint extends React.Component {
                     </div>
 
                     <div className="input-group">
-                        <label htmlFor="email">Coordenada canto superior esquerdo (Longitude)</label>
+                        <label htmlFor="email">Coordenadas</label>
 
                         <input
                             type="text"
-                            id="coord1_1"
-                            name="coord1_1"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto superior esquerdo (Latitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord1_2"
-                            name="coord1_2"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto superior direito (Longitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord2_1"
-                            name="coord2_1"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto superior direito (Latitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord2_2"
-                            name="coord2_2"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto inferior direito (Longitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord3_1"
-                            name="coord3_1"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto inferior direito (Latitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord3_2"
-                            name="coord3_2"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto inferior esquerdo (Longitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord4_1"
-                            name="coord4_1"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada canto inferior esquerdo (Latitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord4_2"
-                            name="coord4_2"
-                            placeholder="Coordenada"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada centro (Latitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord5_1"
-                            name="coord5_1"
-                            placeholder="Coordenada centro"
-                            className="login-input"
-                        ></input>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Coordenada centro (Longitude)</label>
-
-                        <input
-                            type="text"
-                            id="coord5_2"
-                            name="coord5_2"
-                            placeholder="Coordenada centro"
+                            id="coordinates"
+                            name="coordinates"
+                            placeholder="Coordenadas"
                             className="login-input"
                         ></input>
                     </div>
@@ -238,8 +143,16 @@ class InsertPoint extends React.Component {
                         Adicionar Ponto
               </button>
                 </div>
-
-
+                {this.state.success === true && (
+                    <div>
+                        <p>Ponto inserido com sucesso!</p>
+                    </div>
+                )}
+                {this.state.success === false && (
+                    <div>
+                        <p>Erro! Dados mal inseridos</p>
+                    </div>
+                )}
             </div>
         );
     }
