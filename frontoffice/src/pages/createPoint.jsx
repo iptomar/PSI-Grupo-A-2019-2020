259588dda71bf47778ac9b6aff3631e1b3c5021d
Goncalo Rocha <48484505+gonzaleZayy@users.noreply.github.com>
2020-04-18 17:24:14 +0100

    Criar pontos com imagens
    Ainda tem erros. Corrigir mais tarde

--- frontoffice/src/pages/createPoint.jsx
+++ frontoffice/src/pages/createPoint.jsx
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
-
+import ReactFileReader from 'react-file-reader';
 
 class InsertPoint extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             redirect: "",
-            success: null
+            success: null,
+            images: [],
+            imageSuccess: null
         };
 
     }
 
     async submitPoint() {
+        //Obter os valores de cada componente
         let titulo = document.getElementById("title").value;
         let description = document.getElementById("descr").value;
         let tipoEdificio = document.getElementById("tipoEdif").value;
         let dataEdif = document.getElementById("date").value;
         let coordinates = document.getElementById("coordinates").value;
-
-        if(titulo == "" || description == "" || tipoEdificio == "" || dataEdif=="" || coordinates == ""){
+        //Caso algum deles não esteja preenchido não deixa
+        if (titulo == "" || description == "" || tipoEdificio == "" || dataEdif == "" || coordinates == "") {
             this.setState({ success: false });
             return;
         }
-
+        //Dados do utilizador
         let user = JSON.parse(sessionStorage.getItem("userData"));
-
+        //Inserir o ponto
         var myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Content-type", "application/json");
@@ -53,19 +56,75 @@ class InsertPoint extends React.Component {
             "https://localhost:3000/points/insert",
             requestOptions
         )
-
+        //Resposta por parte do server
         let data = await response.json();
-
-        console.log(data);
-
+        //
+        console.log(data.sucess);
+        //Caso consiga inserir, inserir agora as imagens
         if (data.sucess === true) {
+            //Tamanho do array
+            let arraySize = this.state.images.length;
+            for (let index = 0; index < arraySize; index++) {
+                //Obter o index da virgula
+                let virgulaIndex = this.state.images[index].indexOf(',', 0);
+                console.log(virgulaIndex);
+                //Adicionar 1 ao valor do index obtido, pois quer-se o que está à frente
+                //da virgula
+                virgulaIndex += 1;
+                //Obter agora a substring da imagem
+                let image = this.state.images[index].substring(virgulaIndex);
+                image = '"' + image + '"';
+                console.log(image);
+                //Mandar a imagem para o servidor
+                var myHeaders = new Headers();
+                myHeaders.append("Accept", "application/json");
+                myHeaders.append("Content-type", "application/json");
+
+                var raw = JSON.stringify({
+                    data: {
+                        dados: {
+                            Path: "",
+                            Legenda: "Legenda",
+                            AutorFonte: "AutorFonte",
+                            Interesse_id: 1,
+                            usersid: 1,
+                        },
+                        imagem: "ERRO AQUI",
+                    },
+                });
+
+
+
+                var requestOptions = {
+                    method: "POST",
+                    headers: myHeaders,
+                    body: raw,
+                    mode: "cors",
+                    redirect: "follow"
+                };
+
+                let response = await fetch(
+                    "https://localhost:3000/images/insert",
+                    requestOptions
+                )
+                //Resposta por parte do server
+                let data = await response.json();
+
+                console.log(data);
+            }
             this.setState({ success: true });
         } else {
             this.setState({ success: false });
         }
     }
 
+    handleFiles = files => {
+        //Set state imagens com os ficheiros a ser introduzidos
+        this.setState({ images: files.base64 });
+    }
+
     render() {
+
         return (
             <div className="inner-container">
                 <Link type="button" to="/home">
@@ -110,6 +169,13 @@ class InsertPoint extends React.Component {
                             placeholder="Tipo de edificio"
                             className="login-input"
                         ></input>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="email">Fotografias do edificio</label>
+                        <ReactFileReader base64={true} multipleFiles={true} handleFiles={this.handleFiles}>
+                            <button className='btn'>Upload</button>
+                        </ReactFileReader>
                     </div>
 
                     <div className="input-group">
