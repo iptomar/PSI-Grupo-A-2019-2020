72fecda8d82b6ccd24d0b8ef455bb09cfdcbf5bb
Miguel Pereira <aluno18561@ipt.pt>
2020-05-04 12:03:02 +0100

    Insert, update and delete endpoints for routes

--- Backoffice/routes/routes.js
+++ Backoffice/routes/routes.js
@@ -31,13 +31,13 @@ router.get("/list", async function(req, res, next){
 });
 
 //Usage:
-//body.data = { name:"name", work:"work", user_id:"user_id" }
+//body.data = { nome:"nome", descricao:"descrição", user_id:"user_id" }
 router.post("/insert", async function(req, res, next){
 
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
-  await knex("prop")
+  await knex("Roteiro")
   .insert(req.body.data)
   .catch(async function(err) {
     var d = new Date();
@@ -51,58 +51,89 @@ router.post("/insert", async function(req, res, next){
     console.log(err);
   });
 
-  let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
+  let errormesage= {sucess: true, mesage: "Route sucessfully inserted"};
   res.send(errormesage);
 });
 
 //Usage:
-//body.data = { name:"name", work:"work", user_id:"user_id" }
+//body.id = id do proprietário a actualizar
+//body.data = informação a actualizar(json)
 router.post("/update", async function(req, res, next){
+  //ToDo: 
+  //- Terá de ser verificado se o utilizador a solicitar o update é um administrador ou o criador do roteiro
+  //- Não poderá ser permitido o update ao ID do roteiro
 
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
-  await knex("prop")
-  .insert(req.body.data)
-  .catch(async function(err) {
-    var d = new Date();
-    await file(
-      "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
-      "a",
-      err.stack()
-    );
-    let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
+  let upd = false;
+
+  //Verificar se o ponto existe
+  await knex('Roteiro')
+  .where({id: req.body.id})
+  .then(result => {
+      if(!result.length == 0){
+          upd = true;
+      } else {
+          let errormesage = {sucess: false, mesage: "Route doesn't exist"};
+          res.send(errormesage);
+      }
+  })
+  .catch(async function(err){
+      await file("error/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+d.getUTCHours()+"_"+d.getUTCMinutes()+"_"+d.getUTCSeconds(), "a",""+err.stack);
+    let errormesage = { sucess : false , mesage: "token not used" };
     res.send(errormesage);
-    console.log(err);
   });
 
-  let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
+  //Actualiza o ponto
+  if(upd){
+      await knex('Roteiro')
+      .where({id: req.body.id})
+      .update(req.body.data);
+  }
+
+  let errormesage= {sucess: true, mesage: "Route sucessfully updated"};
   res.send(errormesage);
 });
 
 
 //Usage:
-//body.data = { name:"name", work:"work", user_id:"user_id" }
-router.post("/delete", async function(req, res, next){
+//body.data = id do roteiro a eliminar(json)
+router.delete("/delete", async function(req, res, next){
+  //ToDo: Terá de ser verificado se o utilizador a solicitar o delete é
+  //um administrador ou o utilizador que o criou
 
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
-  await knex("prop")
-  .insert(req.body.data)
-  .catch(async function(err) {
-    var d = new Date();
-    await file(
-      "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
-      "a",
-      err.stack()
-    );
-    let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
+  let del = false;
+
+  //Verificar se o ponto existe
+  await knex('Roteiro')
+  .where({id: req.body.id})
+  .then(result => {
+      if(!result.length == 0){
+          del = true;
+      } else {
+          let errormesage = {sucess: false, mesage: "Route doesn't exist"};
+          res.send(errormesage);
+      }
+  })
+  .catch(async function(err){
+      await file("error/"+d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+d.getUTCHours()+"_"+d.getUTCMinutes()+"_"+d.getUTCSeconds(), "a",""+err.stack);
+    let errormesage = { sucess : false , mesage: "token not used" };
     res.send(errormesage);
-    console.log(err);
   });
 
-  let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
+  
+  //Eliminar o ponto
+  if(del){
+    await knex('Roteiro')
+    .where({id: req.body.id})
+    .del();
+  }
+
+  let errormesage= {sucess: true, mesage: "Route sucessfully deleted"};
   res.send(errormesage);
 });
 
