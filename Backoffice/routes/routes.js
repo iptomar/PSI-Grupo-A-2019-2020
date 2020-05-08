541be22a80515fb9e3d4217cf937e455a9fba366
Difficulty01 <bruno.silva.rosario@gmail.com>
2020-05-08 23:49:57 +0100

    fix de erros nas rotas
    uma resolução devido a lógica das rotas

--- Backoffice/routes/routes.js
+++ Backoffice/routes/routes.js
@@ -22,7 +22,7 @@ router.get("/list", async function(req, res, next){
     await file(
       "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
       "a",
-      err.stack()
+      err.stack
     );
     let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
     res.send(errormesage);
@@ -33,7 +33,7 @@ router.get("/list", async function(req, res, next){
 //Usage:
 //body.data = { name:"name", work:"work", user_id:"user_id" }
 router.post("/insert", async function(req, res, next){
-
+ var aux = true;
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
@@ -44,21 +44,24 @@ router.post("/insert", async function(req, res, next){
     await file(
       "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
       "a",
-      err.stack()
+      err.stack
     );
+    aux=!aux;
     let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
     res.send(errormesage);
     console.log(err);
   });
+if(aux){
 
   let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
   res.send(errormesage);
+}
 });
 
 //Usage:
 //body.data = { name:"name", work:"work", user_id:"user_id" }
 router.post("/update", async function(req, res, next){
-
+  var aux = true;
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
@@ -69,22 +72,25 @@ router.post("/update", async function(req, res, next){
     await file(
       "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
       "a",
-      err.stack()
+      err.stack
     );
+    aux=!aux;
+    
     let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
     res.send(errormesage);
     console.log(err);
   });
-
+  if(aux){
   let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
   res.send(errormesage);
+  }
 });
 
 
 //Usage:
 //body.data = { name:"name", work:"work", user_id:"user_id" }
 router.post("/delete", async function(req, res, next){
-
+  var aux = true;
   //Activar chaves estrangeiras
   await knex.schema.raw('PRAGMA foreign_keys = ON;');
 
@@ -95,15 +101,17 @@ router.post("/delete", async function(req, res, next){
     await file(
       "logs/" + d.getFullYear() + "_" + d.getMonth() + "_" + d.getDate(),
       "a",
-      err.stack()
+      err.stack
     );
+    aux=!aux;
     let errormesage = { sucess : false , mesage: "something went wrong and we are working on it" };
     res.send(errormesage);
     console.log(err);
   });
-
+  if(aux){
   let errormesage= {sucess: true, mesage: "Point sucessfully inserted"};
   res.send(errormesage);
+  }
 });
 
 
