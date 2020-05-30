5d1af4ea0a3f96f15f4cf29b6e4e3f74b534f1ff
Difficulty01 <bruno.silva.rosario@gmail.com>
2020-05-30 15:04:48 +0100

    fix utilizadores
    valtava declarar a função no header

--- Backoffice/routes/users.js
+++ Backoffice/routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var knex = require("../utils/databaseConection");
-const {file } = require('../helpers')
+const {file,validation } = require('../helpers')
 
 /* GET users listing. */
 router.get("/", async function(req, res, next) {
